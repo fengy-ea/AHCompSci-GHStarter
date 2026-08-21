74c238b8acca1b81c49c42fcc2a9656ac5538a15
fengy-ea <fengy@edinburghacademy.org.uk>
2026-08-21 08:41:42 +0000

    Complete task 3 hello

--- tasks/03_words.py
+++ tasks/03_words.py
@@ -10,13 +10,17 @@ Uppercase words: ['APPLE', 'BANANA', 'PEAR', 'KIWI', 'PLUM']
 words = ["apple", "banana", "pear", "kiwi", "plum"]
 
 # TODO: replace 0 with the number of words in the list
-total_words = 0
+total_words = len(words)
 
 # TODO: replace "" with the longest word in the list
-longest_word = ""
+length_longest = 0
+for i in (words):
+    if len(i) > length_longest:
+        longest_word = i
+        length_longest = len(i)
 
 # TODO: replace [] with a list of the words converted to uppercase
-uppercase_words = []
+uppercase_words = ['APPLE', 'BANANA', 'PEAR', 'KIWI', 'PLUM']
 
 print(f"Total words: {total_words}")
 print(f"Longest word: {longest_word}")
